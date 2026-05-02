ab52743ddaef159ba814e96e6c824bb6dc9bfac4
Daniel Palmer <daniel@thingy.jp>
2026-05-02 21:50:41 +0900

    Probing the connectors

--- smoldrm.h
+++ smoldrm.h
@@ -7,6 +7,13 @@
 
 #define SMOLDRM_CAST_FROM_DRM_PTR(_p) ((void *)((uintptr_t) _p))
 #define SMOLDRM_CAST_TO_DRM_PTR(_p) ((uint64_t) _p)
+#define SMOLDRM_U32_AT_INDEX(_list, _index) (((uint32_t *) SMOLDRM_CAST_FROM_DRM_PTR(_list))[_index])
+
+#define smoldrm_foreach_u32(_list, _count, _val) \
+	for (int __i = 0, _val = SMOLDRM_U32_AT_INDEX(_list, 0); __i < _count; __i++, _val = SMOLDRM_U32_AT_INDEX(_list, __i))
+
+#define smoldrm_foreach_res_conn(__res, __val) \
+		smoldrm_foreach_u32((__res)->connector_id_ptr, (__res)->count_connectors, __val)
 
 static int smoldrm_open(const char *card)
 {
@@ -36,6 +43,14 @@ static void smoldrm_freeresources(struct drm_mode_card_res *res)
 	free(SMOLDRM_CAST_FROM_DRM_PTR(res->fb_id_ptr));
 }
 
+static void smoldrm_cleanupresources(struct drm_mode_card_res *res)
+{
+	if (res)
+		smoldrm_freeresources(res);
+}
+
+#define __smoldrm_cleanup_resources __attribute__((cleanup(smoldrm_cleanupresources)))
+
 static int smoldrm_getresources(int card, struct drm_mode_card_res *res)
 {
 	uint32_t num_connector;
@@ -60,7 +75,7 @@ static int smoldrm_getresources(int card, struct drm_mode_card_res *res)
 	/*
 	 * This looks a bit weird. We are allocating memory for lists
 	 * of ids for each of these, not the structs that contain the
-	 * data. The ids are u64s.
+	 * data. The ids are u64s?
 	 */
 	res->connector_id_ptr =
 		SMOLDRM_CAST_TO_DRM_PTR(calloc(num_connector, sizeof(uint64_t)));
@@ -85,6 +100,69 @@ static int smoldrm_getresources(int card, struct drm_mode_card_res *res)
 	if (ret) {
 		smoldrm_freeresources(res);
 		return ret;
+	}
+
+	return 0;
+}
+
+static void smoldrm_freeconnector(struct drm_mode_get_connector *conn)
+{
+	free(SMOLDRM_CAST_FROM_DRM_PTR(conn->modes_ptr));
+	free(SMOLDRM_CAST_FROM_DRM_PTR(conn->props_ptr));
+	free(SMOLDRM_CAST_FROM_DRM_PTR(conn->prop_values_ptr));
+	free(SMOLDRM_CAST_FROM_DRM_PTR(conn->encoders_ptr));
+}
+
+static void smoldrm_cleanupconnector(struct drm_mode_get_connector *conn)
+{
+	if (conn)
+		smoldrm_freeconnector(conn);
+}
+
+#define __smoldrm_cleanup_connector __attribute__((cleanup(smoldrm_cleanupconnector)))
+
+static int smoldrm_getconnector(int card,
+				uint32_t conn_id,
+				struct drm_mode_get_connector *conn)
+{
+	uint32_t num_encoder;
+	uint32_t num_mode;
+	uint32_t num_prop;
+	int ret;
+
+	conn->connector_id = conn_id;
+	ret = ioctl(card, DRM_IOCTL_MODE_GETCONNECTOR, conn);
+	if (ret < 0)
+		return -1;
+
+	num_mode = conn->count_modes;
+	num_prop = conn->count_props;
+	num_encoder = conn->count_encoders;
+
+	printf("Connector 0x%08x has %d modes, %d props, %d encoders\n",
+		(unsigned) conn_id, num_mode, num_prop, num_encoder);
+
+	conn->modes_ptr = SMOLDRM_CAST_TO_DRM_PTR(calloc(num_mode,
+			sizeof(struct drm_mode_modeinfo)));
+	conn->props_ptr =
+		SMOLDRM_CAST_TO_DRM_PTR(calloc(num_prop, sizeof(uint64_t)));
+	conn->prop_values_ptr =
+		SMOLDRM_CAST_TO_DRM_PTR(calloc(num_prop, sizeof(uint64_t)));
+	conn->encoders_ptr =
+		SMOLDRM_CAST_TO_DRM_PTR(calloc(num_encoder, sizeof(uint64_t)));
+
+	if (!conn->modes_ptr ||
+	    !conn->props_ptr ||
+	    !conn->prop_values_ptr ||
+	    !conn->encoders_ptr) {
+		smoldrm_freeconnector(conn);
+		return -ENOMEM;
+	}
+
+	ret = ioctl(card, DRM_IOCTL_MODE_GETCONNECTOR, conn);
+	if (ret < 0) {
+		smoldrm_freeconnector(conn);
+		return -1;
 	}
 
 	return 0;
--- smoldrm_test.c
+++ smoldrm_test.c
@@ -2,9 +2,10 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	struct drm_mode_card_res res = { 0 };
+	struct drm_mode_card_res __smoldrm_cleanup_resources res = { 0 };
 	int card;
 	int ret;
+	uint32_t conn_id;
 
 	ret = smoldrm_open(NULL);
 	if (ret < 0) {
@@ -18,6 +19,12 @@ int main(int argc, char **argv, char **envp)
 	if (ret) {
 		printf("smoldrm_getresources() failed: %d\n", ret);
 		return 1;
+	}
+
+	smoldrm_foreach_res_conn(&res, conn_id) {
+		struct drm_mode_get_connector __smoldrm_cleanup_connector conn;
+
+		smoldrm_getconnector(card, conn_id, &conn);
 	}
 
 	return 0;
